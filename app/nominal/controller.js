9d2e44b14fb9c8ecfd3583e0fd9a8a6bfb7a5387
sulthan rafi hendriansyah <sulthan19@gmail.com>
2024-09-20 21:02:27 +0700

    Add feature to update nominal page (CRUD functionality).

--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -29,12 +29,49 @@ module.exports = {
   },
   actionCreate: async (req, res) => {
     try {
-      const { coinName,coinQuantity,price } = req.body;
+      const { coinName, coinQuantity, price } = req.body;
 
-      let nominal = await Nominal({ coinName,coinQuantity,price });
+      let nominal = await Nominal({ coinName, coinQuantity, price });
       await nominal.save();
 
       req.flash("alertMessage", "Berhasil tambah nominal");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/nominal");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const nominal = await Nominal.findOne({ _id: id });
+
+      res.render("admin/nominal/edit", {
+        nominal,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { coinName, coinQuantity, price } = req.body;
+
+      await Nominal.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        { coinName, coinQuantity, price }
+      );
+
+      req.flash("alertMessage", "Berhasil ubah nominal");
       req.flash("alertStatus", "success");
 
       res.redirect("/nominal");
--- app/nominal/router.js
+++ app/nominal/router.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { index, viewCreate, actionCreate } = require("./controller");
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  viewEdit,
+  actionEdit,
+} = require("./controller");
 
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", actionCreate);
+router.get("/edit/:id", viewEdit);
+router.put("/edit/:id", actionEdit);
 
 module.exports = router;
-
