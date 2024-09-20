e9bbd7f6478851f51186bce785d1e3ef5289206d
sulthan rafi hendriansyah <sulthan19@gmail.com>
2024-09-20 23:54:39 +0700

    Implement voucher page management (CRUD) - add create functionality.

---
+++ app/voucher/router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { index, viewCreate, actionCreate } = require("./controller");
+const multer = require("multer");
+const os = require("os");
+
+router.get("/", index);
+router.get("/create", viewCreate);
+router.post(
+  "/create",
+  multer({ dest: os.tmpdir() }).single("image"),
+  actionCreate
+);
+// router.get("/edit/:id", viewEdit);
+// router.put("/edit/:id", actionEdit);
+// router.delete("/delete/:id", actionDelete);
+
+module.exports = router;
