3402b26f6d66bd4e745dcd4501b53e467a030919
sulthan rafi hendriansyah <sulthan19@gmail.com>
2024-09-21 21:20:48 +0700

    Add functionality to update new bank entries in the bank management page (CRUD).

--- app/bank/controller.js
+++ app/bank/controller.js
@@ -6,7 +6,7 @@ module.exports = {
       const alertMessage = req.flash("alertMessage");
       const alertStatus = req.flash("alertStatus");
       const alert = { message: alertMessage, status: alertStatus };
-      const bank = await Bank.find()
+      const bank = await Bank.find();
 
       res.render("admin/bank/view_bank", {
         bank,
@@ -44,121 +44,40 @@ module.exports = {
       res.redirect("/bank");
     }
   },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+
+      res.render("admin/bank/edit", {
+        bank,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, nameBank, noRekening } = req.body;
+
+      await Bank.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        { name, nameBank, noRekening }
+      );
+
+      req.flash("alertMessage", "Berhasil ubah bank");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/bank");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
 };
-
-//   actionEdit: async (req, res) => {
-//     try {
-//       const { id } = req.params;
-//       const { name, category, nominals } = req.body;
-
-//       if (req.file) {
-//         let tmp_path = req.file.path;
-//         let originalExt =
-//           req.file.originalname.split(".")[
-//             req.file.originalname.split(".").length - 1
-//           ];
-//         let filename = req.file.filename + "." + originalExt;
-//         let target_path = path.resolve(
-//           config.rootPath,
-//           `public/uploads/${filename}`
-//         );
-
-//         const src = fs.createReadStream(tmp_path);
-//         const dest = fs.createWriteStream(target_path);
-
-//         src.pipe(dest);
-
-//         src.on("end", async () => {
-//           try {
-//             const voucher = await Voucher.findOne({ _id: id });
-
-//             let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnail}`;
-//             if (fs.existsSync(currentImage)) {
-//               fs.unlinkSync(currentImage);
-//             }
-//             await Voucher.findOneAndUpdate(
-//               {
-//                 _id: id,
-//               },
-//               {
-//                 name,
-//                 category,
-//                 nominals,
-//                 thumbnail: filename,
-//               }
-//             );
-//             req.flash("alertMessage", "Berhasil ubah voucher");
-//             req.flash("alertStatus", "success");
-//             res.redirect("/voucher");
-//           } catch (err) {
-//             req.flash("alertMessage", `${err.message}`);
-//             req.flash("alertStatus", "danger");
-//             res.redirect("/voucher");
-//           }
-//         });
-//       } else {
-//         await Voucher.findOneAndUpdate(
-//           {
-//             _id: id,
-//           },
-//           {
-//             name,
-//             category,
-//             nominals,
-//           }
-//         );
-//         req.flash("alertMessage", "Berhasil ubah voucher");
-//         req.flash("alertStatus", "success");
-//         res.redirect("/voucher");
-//       }
-//     } catch (err) {
-//       req.flash("alertMessage", `${err.message}`);
-//       req.flash("alertStatus", "danger");
-//       res.redirect("/nominal");
-//     }
-//   },
-//   actionDelete: async (req, res) => {
-//     try {
-//       const { id } = req.params;
-//       const voucher = await Voucher.findOneAndDelete({
-//         _id: id,
-//       });
-
-//       let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnail}`;
-//       if (fs.existsSync(currentImage)) {
-//         fs.unlinkSync(currentImage);
-//       }
-
-//       req.flash("alertMessage", "Berhasil hapus voucher");
-//       req.flash("alertStatus", "success");
-
-//       res.redirect("/voucher");
-//     } catch (err) {
-//       req.flash("alertMessage", `${err.message}`);
-//       req.flash("alertStatus", "danger");
-//       res.redirect("/voucher");
-//     }
-//   },
-//   actionStatus: async (req, res) => {
-//     try {
-//       const { id } = req.params;
-
-//       let voucher = await Voucher.findOne({ _id: id });
-//       let status = voucher.status === "Y" ? "N" : "Y";
-
-//       voucher = await Voucher.findOneAndUpdate(
-//         {
-//           _id: id,
-//         },
-//         { status }
-//       );
-
-//       req.flash("alertMessage", "Berhasil ubah status");
-//       req.flash("alertStatus", "success");
-//       res.redirect("/voucher");
-//     } catch (err) {
-//       req.flash("alertMessage", `${err.message}`);
-//       req.flash("alertStatus", "danger");
-//       res.redirect("/voucher");
-//     }
-//   },
--- app/bank/router.js
+++ app/bank/router.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { index, viewCreate, actionCreate } = require("./controller");
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  viewEdit,
+  actionEdit,
+} = require("./controller");
 
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", actionCreate);
-// router.get("/edit/:id", viewEdit);
-// router.put("/edit/:id", actionEdit);
+router.get("/edit/:id", viewEdit);
+router.put("/edit/:id", actionEdit);
 // router.delete("/delete/:id", actionDelete);
 
 module.exports = router;
