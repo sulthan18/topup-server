4a2dd445a10bd7b9c1e395e52a47b3005588c6d8
sulthan rafi hendriansyah <sulthan19@gmail.com>
2024-09-21 22:40:56 +0700

    Implement payment page management with create functionality (CRUD).

---
+++ app/payment/controller.js
@@ -0,0 +1,52 @@
+const Payment = require("./model");
+const Bank = require("../bank/model");
+
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
+      const payment = await Payment.find();
+
+      res.render("admin/payment/view_payment", {
+        payment,
+        alert,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      const banks = await Bank.find();
+      res.render("admin/payment/create", {
+        banks,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { banks, type } = req.body;
+      let payment = await Payment({
+        banks,
+        type,
+      });
+      await payment.save();
+
+      req.flash("alertMessage", "Berhasil tambah payment");
+      req.flash("alertStatus", "success");
+      res.redirect("/payment");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/payment");
+    }
+  },
+};
---
+++ app/payment/router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  //   viewEdit,
+  //   actionEdit,
+  //   actionDelete,
+} = require("./controller");
+
+router.get("/", index);
+router.get("/create", viewCreate);
+router.post("/create", actionCreate);
+// router.get("/edit/:id", viewEdit);
+// router.put("/edit/:id", actionEdit);
+// router.delete("/delete/:id", actionDelete);
+
+module.exports = router;
