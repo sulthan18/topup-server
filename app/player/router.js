122c311474885d26b3ed4de9ec0fbaae787871ef
sulthan rafi hendriansyah <sulthan19@gmail.com>
2024-09-28 11:18:14 +0700

    Add API endpoint for category creation.

--- app/player/router.js
+++ app/player/router.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { landingPage, detailPage } = require("./controller");
+const { landingPage, detailPage, category } = require("./controller");
 const router = express.Router();
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
+router.get("/category", category);
 
 module.exports = router;
