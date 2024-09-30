066b40e73eb7473872bf9c15d0de1c207facb7ed
sulthan rafi hendriansyah <sulthan19@gmail.com>
2024-09-30 21:27:00 +0700

    Implement checkout API functionality.

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,12 +1,42 @@
+const config = require("../../config");
+const jwt = require("jsonwebtoken");
+const Player = require("../player/model");
+
 module.exports = {
-    isLoginAdmin: (req, res, next) => {
-      if (!req.session.user) {
-        req.flash("alertMessage", "Mohon maaf, sesi Anda telah habis. Silahkan login kembali.");
-        req.flash("alertStatus", "danger");
-        return res.redirect("/");
+  isLoginAdmin: (req, res, next) => {
+    if (!req.session.user) {
+      req.flash(
+        "alertMessage",
+        "Mohon maaf, sesi Anda telah habis. Silahkan login kembali."
+      );
+      req.flash("alertStatus", "danger");
+      return res.redirect("/");
+    }
+
+    next();
+  },
+
+  isLoginPlayer: async (req, res, next) => {
+    try {
+      const authorizationHeader = req.headers.authorization;
+      const token = authorizationHeader ? authorizationHeader.replace("Bearer ", "") : null;
+
+      if (!token) {
+        return res.status(401).json({ error: "No token provided. Access denied." });
       }
-      
+
+      const data = jwt.verify(token, config.jwtKey);
+      const player = await Player.findOne({ _id: data.player.id });
+
+      if (!player) {
+        return res.status(401).json({ error: "Player not found. Access denied." });
+      }
+
+      req.player = player;
+      req.token = token;
       next();
-    },
-  };
-  
+    } catch (err) {
+      res.status(401).json({ error: "Not authorized to access this resource." });
+    }
+  },
+};
--- app/player/router.js
+++ app/player/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { landingPage, detailPage, category } = require("./controller");
+const { landingPage, detailPage, category, checkout } = require("./controller");
+const { isLoginPlayer } = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
+router.post("/checkout", isLoginPlayer, checkout);
 
 module.exports = router;
